8d351e221ef37610b557c4eb7eb66728f3f63d26
Nguyen Thanh Long <l002591@cybozu.onmicrosoft.com>
2022-09-24 01:17:47 +0700

    feature/login: add sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Login from './pages/login';
+import { Routes, Route } from 'react-router-dom';
+import NotFoundPage from './pages/not.found';
+import SignUp from './pages/sign.up';
 
 const App = () => {
   return (
-    <Login />
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
   );
-}
+};
 
 export default App;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,38 +1,37 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Login: React.FC = () => {
   return (
     <>
-      <div className="text-6xl font-bold text-center my-2 text-titleLogin">
+      <div className='text-6xl font-bold text-center my-2 text-titleLogin'>
         Kanban
       </div>
-      <div className="flex justify-center my-6">
-        <div className="w-96 justify-center border boder-cyan-50 shadow-md p-5 rounded bg-white">
-          <div className="text-center mb-3 text-lg font-semibold">
+      <div className='flex justify-center my-6'>
+        <div className='w-96 justify-center border boder-cyan-50 shadow-lg p-5 rounded bg-white'>
+          <div className='text-center mb-3 text-lg font-semibold'>
             Login to Kanban
           </div>
           <div>
+            <input className='input-login' type='text' placeholder='Username' />
             <input
-              className="input-login"
-              type="text"
-              placeholder="Username"
+              className='input-login'
+              type='password'
+              placeholder='Password'
             />
-            <input
-              className="input-login"
-              type="text"
-              placeholder="Password"
-            />
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">
+            <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full'>
               Login
             </button>
-            <div className="relative flex py-5 items-center">
-              <div className="flex-grow border-t border-gray-400"></div>
-              <span className="flex-shrink mx-4 text-gray-400">OR</span>
-              <div className="flex-grow border-t border-gray-400"></div>
+            <div className='relative flex py-5 items-center'>
+              <div className='flex-grow border-t border-gray-400'></div>
+              <span className='flex-shrink mx-4 text-gray-400'>OR</span>
+              <div className='flex-grow border-t border-gray-400'></div>
             </div>
-            <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded w-full">
-              Sign in
-            </button>
+            <Link to='/signup'>
+              <button className='bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded w-full'>
+                Sign up
+              </button>
+            </Link>
           </div>
         </div>
       </div>
