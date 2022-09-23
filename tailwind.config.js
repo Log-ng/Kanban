a15ed684e966ef9f47e235dc5d03ffcf2d0ea302
Nguyen Thanh Long <l002591@cybozu.onmicrosoft.com>
2022-09-23 14:41:35 +0700

    feature/login: add taiwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        loginButon: "#5AAC44",
+      },
+      spacing: {
+        line: "0.0625rem",
+      },
+    },
+  },
+  plugins: [],
+};
