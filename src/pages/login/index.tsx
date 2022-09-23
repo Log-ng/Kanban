45ed588cd8e30358f3edc326bc92048c033e068d
Nguyen Thanh Long <l002591@cybozu.onmicrosoft.com>
2022-09-23 18:32:18 +0700

    feature/login: add page login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
   return (
     <>
-      <div>
-        Long
+      <div className="text-6xl font-bold text-center my-2 text-titleLogin">
+        Kanban
+      </div>
+      <div className="flex justify-center my-6">
+        <div className="w-96 justify-center border boder-cyan-50 shadow-md p-5 rounded bg-white">
+          <div className="text-center mb-3 text-lg font-semibold">
+            Login to Kanban
+          </div>
+          <div>
+            <input
+              className="input-login"
+              type="text"
+              placeholder="Username"
+            />
+            <input
+              className="input-login"
+              type="text"
+              placeholder="Password"
+            />
+            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">
+              Login
+            </button>
+            <div className="relative flex py-5 items-center">
+              <div className="flex-grow border-t border-gray-400"></div>
+              <span className="flex-shrink mx-4 text-gray-400">OR</span>
+              <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+            <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded w-full">
+              Sign in
+            </button>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      segoeUi: ['"Segoe UI"', "sans-serif"],
+    },
     extend: {
       colors: {
         loginButon: "#5AAC44",
+        titleLogin: "#253858",
       },
       spacing: {
         line: "0.0625rem",
