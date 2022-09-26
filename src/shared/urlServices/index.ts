6f3686832fb6b2f7b9b21e7c670aaae0ba585cb3
Nguyen Thanh Long <l002591@cybozu.onmicrosoft.com>
2022-09-26 22:48:57 +0700

    feature/login: change folder name, file

--- src/shared/urlServices/index.ts
+++ src/shared/urlServices/index.ts
@@ -1,6 +1,6 @@
 import myAxios from 'axios';
 import { appRouters } from 'shared/urlResources';
 
-export const LOGIN_API = `http://localhost/Kanban/server/controllers/user/login/index.php`;
+export const LOGIN_API = `http://localhost/Kanban/server/controllers/user/login/login.php`;
 
 export { appRouters, myAxios };
