81cb3d2ac1a195e801447d92071664bd69c680a9
Nguyen Thanh Long <l002591@cybozu.onmicrosoft.com>
2022-09-26 13:12:29 +0700

    feature/login: add delete all ole token then gen new token

---
+++ server/models/token.model.php
@@ -0,0 +1,32 @@
+<?php
+class Token {
+    private $conn;
+    public $table = 'token';
+
+    public $username;
+    public $token;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function saveToken() {
+        $query = "INSERT INTO $this->table (username, token) VALUES (?, ?)";
+        $this->username = htmlspecialchars(strip_tags($this->username));
+        $this->token = htmlspecialchars(strip_tags($this->token));
+
+        $stmt = $this->conn->prepare($query);
+        
+        $stmt->bindParam(1, $this->username);
+        $stmt->bindParam(2, $this->token);
+
+        return $stmt->execute();
+    }
+
+    public function deleteAllOldToken($usernameDelete) {
+        $query = "DELETE FROM $this->table WHERE username='$usernameDelete'";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+    }
+}
